87ee7584b3d67cfbe9ae3917a9118e364dd22b27
IgorS <kerogii@gmail.com>
2022-10-30 22:29:55 +0500

    git init

---
+++ Assets/Scripts/LevelTag.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+
+namespace Mono
+{
+    internal struct LevelTag: IComponentData
+    {
+    }
+}
---
+++ Assets/Scripts/SceneData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Mono
+{
+    internal class SceneData : MonoBehaviour
+    {
+    }
+}
